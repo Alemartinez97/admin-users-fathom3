6ea77ab10f857c66a755f065e74b86e7fea71d22
Alemartinez97 <deyvidofi@gmail.com>
2023-11-24 13:44:01 -0300

    add changes

--- src/utils/hashedPassword.ts
+++ src/utils/hashedPassword.ts
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 export const hashedPassword = async function (password: string) {
     try {
         const response = await bcrypt.hash(password, 10);
-        console.log("response: ", response);
         return response;
     } catch (err) {
         console.error(err);
